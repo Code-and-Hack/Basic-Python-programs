ae51120ef2ef96e75e90b4ca51be60726eee584b
Mohamed Nadeem <107123890+Code-and-Hack@users.noreply.github.com>
2024-10-14 20:50:14 +0300

    Created Variables and Data Types.py
    Variables and Data Types added, please support for more...

---
+++ 1. Introduction to Python/Variables and Data Types.py
@@ -0,0 +1,50 @@
+'''
+1) Before diving very deep, let's understand what is a variable :-
+  · Variables are like containers that store data values.
+  · In Python, you don’t need to specify the type of the variable; it automatically understands based on the value assigned.
+  · Here are the examples...
+'''
+
+x = 10  # Integer variable
+y = 3.14  # Float variable
+name = "Your name"  # String variable
+is_student = True  # Boolean variable
+
+'''
+If you type ' print(type(x)) ' it will show what variable it is using.
+Note: Boolean consist only 'True' or 'False'
+'''
+
+'''
+2) In Python there are many kinds of Data Types, let's learn one by one :-
+  a) Numbers:-
+    · Integers (Whole numbers like 1, 2, 100)
+    · Floats (Decimal numbers like 3.14, 0.001)
+    · Here are some examples...
+'''
+
+age = 16  # Integer
+pi = 3.1415  # Float
+
+'''
+Now here's a quick way to remember, if there is a decimal point, it is a Float or else it is a Integer.
+'''
+'''
+  b) Strings:-
+    · They are the sequence of characters, used for text.
+    · You can use single, double, or triple quotes.
+    · Some examples...
+'''
+
+name = "Nadeem"  # Using double quotes
+greeting = 'Hello!'  # Using single quotes
+paragraph = '''This is
+a multiline
+string.'''  # Using triple quotes
+
+''' More Coming sooooon..... Please support '''
+
+
+
+
+
